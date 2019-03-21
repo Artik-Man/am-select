e6b74080867b6937e089fc0050f437556c9a5ebe
Artik <mail@artik.me>
2019-03-21 17:25:19 +0300

    UPD: styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { } from '@angular/material';
 import { AppSelectComponent } from './components/app-select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from './services/api';
